5593d24a74e9cc18ff9374da8e5ec634656a1092
Kingsley Omoaka-Iyorah <kingsley.omiy@gmail.com>
2024-04-17 11:38:36 +0100

    only commit

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -7,12 +7,12 @@ const Filters = () => {
 
   return (
     <div className="align-page flex flex-col md:flex-row md:items-center gap-3">
-      <div className="flex gap-3 w-fit items-center border border-primary rounded-md p-2 text-primary">
+      <div className="flex gap-3 w-fit items-center border border-primary rounded-md p-3 text-primary">
         <FaFilter />
         Filters (1)
       </div>
       <div>
-        <label className="input input-bordered flex input-sm items-center rounded-md gap-2">
+        <label className="input input-bordered flex items-center rounded-md gap-2">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
           <input type="text" className="grow"/>
         </label>
@@ -20,7 +20,7 @@ const Filters = () => {
       <div className="-mt-4">
         <FormSelect
           list={sortOptions}
-          size='select-sm'
+          size='select-md'
         />
       </div>
     </div>

d667175836a2b0c2f9c79690490b225834fe6739
Kingsley  Omoaka-Iyorah <omoaka-iyorahk@accessbankplc.com>
2024-07-11 02:52:34 +0100

    dashboard page

--- src/components/DashboardCard.jsx
+++ src/components/DashboardCard.jsx
@@ -13,19 +13,19 @@ const DashboardCard = () => {
         <div className="transition-all duration-300 p-4 rounded-2xl bg-blue-900 w-full">
           <div className="grid grid-cols-2 gap-7 p-5">
             <div className="flex flex-col gap-3 text-base-100 border-r border-r-gray-500">
-              <span className="rounded-full bg-gray-400 w-fit p-2 text-base-100 opacity-65">
+              <span className="rounded-full bg-gray-700 w-fit p-2 text-base-100 opacity-75">
                 <FaScroll size={40} />
               </span>
-              <p className="font-light text-gray-300">
+              <p className="font-light text-gray-200">
                 Highest Overall Team Score
               </p>
               <p className="text-3xl">99%</p>
             </div>
             <div className="flex flex-col gap-3 text-base-100">
-              <span className="rounded-full bg-gray-400 w-fit p-2 text-base-100 opacity-65">
+              <span className="rounded-full bg-gray-700 w-fit p-2 text-base-100 opacity-75">
                 <MdScoreboard size={40} />
               </span>
-              <p className="font-light text-gray-300">
+              <p className="font-light text-gray-200">
                 Highest Appraisal Scorer
               </p>
               <p className="text-3xl">Umar T.</p>
